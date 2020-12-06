a78b357569c9fa48671e282299744d3cb948c32b
Jan Licciardello <jan.lic99@live.it>
2020-12-06 01:05:33 +0100

    Provo a caricare il solito progetto

---
+++ identify abbreviations/MainForm.Designer.cs
@@ -0,0 +1,144 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: jlicciardello
+ * Date: 20/11/2020
+ * Time: 11:26
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace identify_abbreviations
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 63);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Trova abbreviazioni da HTML";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 82);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(823, 475);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(682, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 63);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Salva in excel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(256, 35);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(110, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "punto e spazio [. ]";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(256, 12);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(110, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "spazio e punto [ .]";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(256, 59);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(101, 17);
+            this.checkBox3.TabIndex = 5;
+            this.checkBox3.Text = "punto singolo [.]";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(163, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Tag di inclusione";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(847, 569);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "MainForm";
+            this.Text = "identify abbreviations";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		internal System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ identify abbreviations/MainForm.cs
@@ -0,0 +1,178 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: jlicciardello
+ * Date: 20/11/2020
+ * Time: 11:26
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+using System.Net;
+using System.IO	;
+
+using ExcelLibrary.CompoundDocumentFormat;
+using ExcelLibrary.SpreadSheet;
+
+namespace identify_abbreviations
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		String ris = "";
+		void Button1Click(object sender, EventArgs e)
+		{
+			OpenFileDialog ofd = new OpenFileDialog();
+			ofd.Filter = "HTML|*.html";
+			
+			if(ofd.ShowDialog() == DialogResult.OK)
+			{
+				richTextBox1.Clear();
+				//INIZIALIZZAZIONE VARIABILI PER FILTRI CON CHECKBOX
+				string puntoSpazio = "";
+				string spazioPunto = "";
+				string puntoSingolo = "";
+				if (checkBox1.Checked) {
+					puntoSpazio = ". ";
+				}
+				if (checkBox2.Checked) {
+					spazioPunto = " .";
+				}
+				if (checkBox3.Checked) {
+					puntoSingolo = ".";
+				}
+				//--------------------------------------------------
+
+				string filePath = ofd.FileName;			//OTTIENE LA STRINGA CORRISPONDENTE AL PERCORSO DEL FILE SELEZIONATO
+				//----------CODICE-PER-PRENDERE-IL-SORGENTE-DELL'HTML-------------
+				FileWebRequest request=(FileWebRequest)WebRequest.Create(filePath);
+				FileWebResponse response=(FileWebResponse)request.GetResponse();
+				StreamReader sr=new StreamReader(response.GetResponseStream());	
+				String htmlSource = sr.ReadToEnd();
+				//----------------------------------------------------------------
+				//Dentro htmlSource c'è una il sorgente
+
+				//string[] testSplit = htmlSource.Split("<tr id=\"r1\">");
+				string[] htmlSplitted = null;
+				htmlSplitted = htmlSource.Split(new[] { "<tr id=" }, StringSplitOptions.None);
+
+
+
+				//richTextBox1.Text = htmlSplitted2[0];
+
+				double t = 100/htmlSplitted.Length;
+
+				for(int i=1; i<htmlSplitted.Length; i++){
+					
+					string[] htmlSplitted1 = null;
+					htmlSplitted1 =	htmlSplitted[i].Split(new[] { "<td class=\"td2\">" }, StringSplitOptions.None);
+					//--------------------------------MODIFICHE PER AGGIUNGERE ID E SOURCE-------------------------------------------------
+					string[] htmlSplitSup = null;                                                                   //Variabile che creo per prendere anche id source
+					htmlSplitSup = htmlSplitted1[0].Split(new[] { "</td>" }, StringSplitOptions.None);
+
+					string[] htmlSplitSup2 = null;
+					htmlSplitSup2 = htmlSplitSup[0].Split(new[] { ">" }, StringSplitOptions.None);
+
+					string[] htmlSplitSup3 = null;
+					htmlSplitSup3 = htmlSplitSup[1].Split(new[] { "<td>" }, StringSplitOptions.None);
+
+					string id = htmlSplitSup2[2];
+					string source = htmlSplitSup3[1];
+					//-----------------------------------------------------------------------------------------------------------------
+					if (htmlSplitted1.Length<2){
+					continue;
+					}
+					//LA STRINGA DI TARGHE E' DENTRO htmlSplitted2
+					string[] htmlSplitted2 = null;
+					htmlSplitted2 = htmlSplitted1[1].Split(new[] { "</td>" }, StringSplitOptions.None);
+					
+					
+					if(htmlSplitted2[0].Contains(puntoSpazio) || htmlSplitted2[0].Contains(spazioPunto) || htmlSplitted2[0].Contains(puntoSingolo)){
+						richTextBox1.Text += id + " | " + source + " | " + htmlSplitted2[0] +  "\n";
+					}
+					
+				}
+				//richTextBox1.Text = ris;
+				sr.Close();
+			}
+		}
+	
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+			SaveFileDialog sfd = new SaveFileDialog();
+			//sfd.InitialDirectory = @"C:\";
+			sfd.RestoreDirectory = true;
+			sfd.FileName = "*.xls";
+			sfd.DefaultExt = "Parole abbreviate";
+			sfd.Filter = "Xls files (*.xls)|*.xls";
+			
+			if(sfd.ShowDialog() == DialogResult.OK)
+			{
+			string file = sfd.FileName;
+			Workbook workbook = new Workbook(); 
+			Worksheet worksheet = new Worksheet("Parole abbreviate"); 
+			worksheet.Cells[0, 0] = new Cell("ID");
+			//-----------MODIFICHE PER OTTENERE ID E SOURCE------------------------------------
+			worksheet.Cells[0, 1] = new Cell("SOURCE");
+			worksheet.Cells[0, 2] = new Cell("TARGHET");
+
+			string[] tempSplit = ris.Split(new[] { "\n" }, StringSplitOptions.None);
+			
+
+				for (int i=0; i<tempSplit.Length; i++){
+					//------------------------ROBA AGGIUNTA-------------------------------------------------
+					string[] tempSplit1= null;
+					tempSplit1 = tempSplit[i].Split(new[] { " | " }, StringSplitOptions.None);
+					if (tempSplit1.Length<3) {
+						continue;
+					}
+					worksheet.Cells[i + 1, 0] = new Cell(tempSplit1[0]);
+					worksheet.Cells[i + 1, 1] = new Cell(tempSplit1[1]);
+					worksheet.Cells[i + 1, 2] = new Cell(tempSplit1[2]);
+					//----------------------------------------------------------------------------------------
+				}
+			
+			workbook.Worksheets.Add(worksheet);
+			workbook.Save(file);
+			MessageBox.Show("File excel salvato con successo");
+			}
+			
+
+
+		}
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
